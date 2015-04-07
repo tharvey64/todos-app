49e211b1c63c9e5b105811ad3c028439e0fd35b4
Thomas Harvey <thomasharvey24@gmail.com>
2015-04-07 16:15:27 -0400

    removes previous response before adding a new one

--- exercises/1-deaf-grandpa-ajax/project/deafgrandpa/static/deafgrandpa/grandpa.js
+++ exercises/1-deaf-grandpa-ajax/project/deafgrandpa/static/deafgrandpa/grandpa.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
     $('#say_grandpa').submit(function(event){
         event.preventDefault(); 
-        $('.grandpa').remove('p')
         $.post( "/", $( "#say_grandpa" ).serialize(), function(data){
+            $('.grandpa p').remove();
+
             var response
             if (data.said.toUpperCase() != data.said){
                 response = "Speak up I cannot hear you."
             }else{
-                response = "I could care less."
+                response = "Go Away."
             }
             $('.grandpa').append("<p>" + response + "</p>")
         });
