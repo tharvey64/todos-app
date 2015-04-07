ed47c7aec2d452d4404de8eff30e711e1dfa6d43
Thomas Harvey <thomasharvey24@gmail.com>
2015-04-07 16:12:32 -0400

    Form input handled in js. Prints p element to screen

--- exercises/1-deaf-grandpa-ajax/project/deafgrandpa/static/deafgrandpa/grandpa.js
+++ exercises/1-deaf-grandpa-ajax/project/deafgrandpa/static/deafgrandpa/grandpa.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-
+    $('#say_grandpa').submit(function(event){
+        event.preventDefault(); 
+        $('.grandpa').remove('p')
+        $.post( "/", $( "#say_grandpa" ).serialize(), function(data){
+            var response
+            if (data.said.toUpperCase() != data.said){
+                response = "Speak up I cannot hear you."
+            }else{
+                response = "I could care less."
+            }
+            $('.grandpa').append("<p>" + response + "</p>")
+        });
+    });
 })
--- exercises/1-deaf-grandpa-ajax/project/deafgrandpa/views.py
+++ exercises/1-deaf-grandpa-ajax/project/deafgrandpa/views.py
@@ -3,9 +3,9 @@ from django.views.generic import View
 from django.http import HttpResponse, JsonResponse
 
 class GrandpaView(View):
-	template = 'deafgrandpa/index.html'
-	def get(self, request):
-		return render(request, self.template)
+    template = 'deafgrandpa/index.html'
+    def get(self, request):
+        return render(request, self.template)
 
-	def post(self, request):
-		pass
+    def post(self, request):
+        return JsonResponse(request.POST)
